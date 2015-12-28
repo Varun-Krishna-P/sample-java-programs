d8d739d911ec1d67eceb152d37b52f18ff8a8ead
Varun Krishna. P <varunkrishna.dbz@gmail.com>
2015-12-28 20:45:08 +0530

    Commiting Increment.java

---
+++ code/practise/Myexample/Increment.java
@@ -0,0 +1,16 @@
+package code.practise.Myexample;
+
+public class Increment {
+
+	/**
+	 * @param args
+	 */
+	public static  int i = 1;	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		i++;
+		System.out.println("The integer value is: "+i);
+
+	}
+
+}
